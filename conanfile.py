3d52220d9d6624dcb9f8b16a3482204b2976f16b
Andre Luiz Clinio <clinio@tecgraf.puc-rio.br>
2020-08-15 19:20:19 -0300

    testing conan...

---
+++ conanfile.py
@@ -0,0 +1,44 @@
+from conans import ConanFile
+from conans import tools
+
+class MacServerConan(ConanFile):
+    name = "mcpserver"
+    version = "0.0.1"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "make"
+    author = "André Luiz Clinio (andre.clinio@gmail.com)"
+    description = "Micro compiled webserver for C++"
+    topics = ("web server", "C++ 11", "http")
+
+    def source(self):
+        self.run("git clone https://github.com/andreclinio/mcpserver.git .")
+        self._delete_post_cloned()
+        
+    def build(self):
+        self.run("git clone https://github.com/andreclinio/mcpserver.git")
+        with tools.chdir("mcpserver/library"):
+            self._delete_post_cloned()
+            cmds = [self._make_program, "build"]
+            command  = " ".join(cmds)
+            self.run(command)
+
+    def package(self):
+        self.copy("*.hpp", dst="include", src="library/include")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["mcpserver"]
+
+    @property
+    def _make_program(self):
+        if self.settings.compiler == "Visual Studio":
+            return "nmake"
+        make_program = tools.get_env("CONAN_MAKE_PROGRAM", tools.which("make") or tools.which('mingw32-make'))
+        make_program = tools.unix_path(make_program) if tools.os_info.is_windows else make_program
+        return make_program
+
+    def _delete_post_cloned(self):
+        delete_dirs = [".git", ".vscode", ".github", "attic", "samples"]
+        for dir in delete_dirs:            
+            tools.rmdir(dir)
